95979752796f04fcedc3b24f0072b27d6ae46749
inspire-coding <+PaZ+PeTi+88113+>
2021-08-10 19:45:58 +0200

    End of section 9

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace API.Controllers
         }
 
         [Authorize]
-        [HttpGet("{id}")]
+        [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
